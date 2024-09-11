4cff1ac926f878a5f48ed9c1f64086d2e81afa71
Ayan Shaw <ayanshw@outlook.com>
2024-09-12 03:25:06 +0530

    signal commit

--- app/src/main/java/com/ayanshw/butterfly/MainActivity.java
+++ app/src/main/java/com/ayanshw/butterfly/MainActivity.java
@@ -1,6 +1,11 @@
 package com.ayanshw.butterfly;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +18,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+        int flags = decorView.getSystemUiVisibility();
+
+        // To display dark text/icons on a light status bar (for transparency)
+        flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+
+        // Apply changes
+        decorView.setSystemUiVisibility(flags);
+
+        // Make the status bar transparent
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
     }
 }
